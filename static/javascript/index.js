7987c9ac5e57c13e54e829e185fc1ea2535c551d
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-07-09 03:33:29 +0500

    Implement the behaviour of chat Interface

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -4,9 +4,32 @@ function toggleChat() {
 
   if (chatBody.style.display === "none") {
     chatBody.style.display = "block";
-    minimizeBtn.textContent = "Contact Admin - ";
+    minimizeBtn.textContent = "-";
   } else {
     chatBody.style.display = "none";
-    minimizeBtn.textContent = "Contact Admin + ";
+    minimizeBtn.textContent = "+";
   }
 }
+var i = 1;
+var send = document.getElementById("send-btn");
+send.addEventListener("click", function () {
+  var message = document.getElementById("input");
+  console.log(message.value);
+  var newDiv = document.createElement("div");
+  newDiv.className = "new-message";
+  var newMessage = document.createElement("p");
+  newMessage.id = "new-message" + i;
+  newDiv.appendChild(newMessage);
+  console.log(newMessage);
+  newMessage.innerHTML = message.value;
+  console.log(newMessage);
+  var chatBody = document.getElementById("chat-messages");
+  if (i === 1) {
+    chatBody.appendChild(newDiv);
+  } else {
+    var lastMessage = document.getElementById("new-message" + (i - 1));
+    lastMessage.append(newDiv);
+  }
+
+  message.value = "";
+});
